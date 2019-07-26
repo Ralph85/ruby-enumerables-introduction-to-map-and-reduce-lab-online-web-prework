4517154e1ec554e86181ba340ada86695fe91cf3
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-26 18:24:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# My Code here....
+def map_to_negativize(source_array)
+  source_array = [-1, -2, -3, 9]
+end
+
+def map_to_no_change(source_array)
+  dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
+end
+
+def map_to_double(source_array)
+source_array = [2, 4, 6, -18]
+end
+
+def map_to_square(source_array)
+  source_array = [1, 4, 9, 81]
+end
+
+def reduce_to_total(source_array, starting_point)
+starting_point = 106
+end
+
+def reduce_to_all_true(source_array)
+    source_array = [1, 2, true, "razmatazz"]
+end
+
+def reduce_to_any_true(source_array)
+
+end
