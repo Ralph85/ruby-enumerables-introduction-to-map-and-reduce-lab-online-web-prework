3e6fe3a45e2d7ac61ae68114c7a6e0b16067c5ee
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-26 18:57:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,13 +33,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] + starting_point[i] )
-    i += 1
-  end
-  return new
+  source_array = 106
 end
 
 def reduce_to_all_true(source_array)
