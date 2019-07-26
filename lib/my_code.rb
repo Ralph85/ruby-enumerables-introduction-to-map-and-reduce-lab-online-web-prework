a88f21e381ccabf39e3cb3acd8bc34bbd8b60bfd
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-26 18:58:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,7 +37,7 @@ def reduce_to_total(source_array, starting_point)
 end
 
 def reduce_to_all_true(source_array)
-    source_array = true
+    source_array = falsey value
 end
 
 def reduce_to_any_true(source_array)
